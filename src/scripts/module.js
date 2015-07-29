c23758eb6b88e8ce8c014df86dab26a74e24fdff
Keyton Stanier <keyton.j.stanier@gmail.com>
2015-07-29 19:27:12 -0400

    Finished renaming, standardized navbar inclusion
    Tied up loose ends related to project name on the backend.  Improved navbar include functionality, moved navbar to page template, insuring consistency and code reuseability for user-based navbar elements
    
    Conflicts:
    	lib/assetManager.js
    	package.json
    	src/scripts/module.js
    	static/css/coyot.io.css
    	static/css/coyot.io.min.css
    	static/js/coyot.io.js
    	views/includes/links.jade
    	views/includes/mixins/navbar.jade
    	views/index.jade
    	views/panel/cluster/manage.jade
    	views/panel/index.jade
    	views/panel/server/packages/install.jade
    	views/panel/server/packages/list.jade
    	views/panel/server/packages/update.jade
    	views/panel/server/view.jade
    	views/templates/page.jade

--- src/scripts/module.js
+++ src/scripts/module.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 var app = angular.module('coyot.io', ['ngRoute', 'ngAnimate']);
+=======
+var app = angular.module('coyot.io', []);
+>>>>>>> 9b57247... Finished renaming, standardized navbar inclusion
--- static/js/coyot.io.js
+++ static/js/coyot.io.js
@@ -4,273 +4,50 @@
  * @link https://github.com/stanier/coyot.io
  * @license MIT
  */
-toastr.options.newestOnTop = false;
-toastr.options.progressBar = false;
-toastr.options.positionClass = 'toast-bottom-right';
+var app = angular.module('coyot.io', []);
 
-$.material.init();
-$.material.ripples();
-$.material.input();
-$.material.checkbox();
-$.material.radio();
+app.controller('ClusterManagementCtlr', ['$scope', '$http', function($scope, $http) {
+    $http.get('/cluster/list')
+    .success(function(data, status, headers, config) {
+        $scope.servers = [];
 
-var app = angular.module('coyot.io', ['ngRoute', 'ngAnimate']);
-
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/cluster/manage', {
-            templateUrl: 'pages/cluster/manage',
-            controller: 'ClusterCtlr'
-        })
-        .when('/management/dashboard', {
-            templateUrl: 'pages/management/dashboard',
-            controller: 'ManagementCtlr'
-        })
-        .when('/management/users', {
-            templateUrl: 'pages/management/users',
-            controller: 'ManagementCtlr'
-        })
-        .when('/server/:hostname/overview', {
-            templateUrl: 'pages/server/overview',
-            controller: 'ServerCtlr',
-            reloadOnSearch: false
-        })
-        .when('/server/:hostname/packages/install', {
-            templateUrl: 'pages/server/packages/install',
-            controller: 'ServerCtlr',
-            reloadOnSearch: false
-        })
-        .when('/server/:hostname/packages/update', {
-            templateUrl: 'pages/server/packages/update',
-            controller: 'ServerCtlr',
-            reloadOnSearch: false
-        })
-        .when('/server/:hostname/packages', {
-            templateUrl: 'pages/server/packages',
-            controller: 'ServerCtlr',
-            reloadOnSearch: false
-        })
-        .when('/server/:hostname/package/:pkg/', {
-            templateUrl: 'pages/server/packages/view',
-            controller: 'ServerCtlr',
-            reloadOnSearch: false
-        })
-        .when('/server/:hostname/services', {
-            templateUrl: 'pages/server/services',
-            controller: 'ServerCtlr',
-            reloadOnSearch: false
-        })
-        .when('/server/:hostname/service/:service/', {
-            templateUrl: 'pages/server/services/view',
-            controller: 'ServerCtlr',
-            reloadOnSearch: false
-        })
-        .when('/server/:hostname/processes', {
-            templateUrl: 'pages/server/processes',
-            controller: 'ServerCtlr',
-            reloadOnSearch: false
-        })
-        .when('/server/:hostname/process/:process', {
-            templateUrl: 'pages/server/processes/view',
-            controller: 'ServerCtlr',
-            reloadOnSearch: false
-        })
-        .otherwise({
-            redirectTo: 'management/dashboard'
-        })
-    ;
-
-    $locationProvider.html5Mode(true);
-}]);
-
-app.run(['$rootScope', '$location', function($rootScope, $location) {
-    $rootScope.path = {
-        equals: function(path) {
-            return path == $location.path();
-        },
-        startsWith: function(path) {
-            return $location.path().startsWith(path);
+        for (var i = 0; i < data.length; i++) {
+            $scope.servers[i] = {
+                hostname: data[i].hostname,
+                host    : data[i].host,
+                port    : data[i].port,
+                isWorker: data[i].type === 'hybrid' || data[i].type === 'worker',
+                isWeb   : data[i].type === 'hybrid' || data[i].type === 'web'
+            };
+            getStats(i);
         }
-    };
-}]);
 
-function createSocket(host, port, callback) {
-    callback(io('http://' + host + ':' + port));
-}
-
-app.controller('ClusterCtlr', ['$scope', '$http', function($scope, $http) {
-    $scope.getServers = function() {
-        $http.get('/api/cluster/servers')
-        .success(function(data, status, headers, config) {
-            $scope.servers = [];
-
-            for (var i = 0; i < data.length; i++) {
-                $scope.servers[i] = {
-                    hostname: data[i].hostname,
-                    host    : data[i].host,
-                    port    : data[i].port,
-                    isWorker: data[i].type === 'hybrid' || data[i].type === 'worker',
-                    isWeb   : data[i].type === 'hybrid' || data[i].type === 'web'
-                };
-                $scope.getStats(i);
-            }
-        })
-        .error(function(data, status, headers, config) {
-            console.log(data);
-        });
-    };
-
-    $scope.getStats = function(index) {
-        $http.get('//' + $scope.servers[index].host + ':' +
-            $scope.servers[index].port + '/api/system/stats?type=simple')
-        .success(function(data, status, headers, config) {
-            $scope.servers[index].online  = data.online;
-            $scope.servers[index].freemem = data.freemem;
-        })
-        .error(function(data, status, headers, config) {
-            console.log(data);
-        });
-    };
-}]);
-
-app.controller('ManagementCtlr', ['$scope', '$http', function($scope, $http) {
-    $scope.pageSize    = 20;
-    $scope.currentPage = 0;
-
-    $scope.getUsers = function() {
-        $http.get('/api/management/users')
+        function getStats(i) {
+            $http.get('//' + $scope.servers[i].host + ':' +
+                $scope.servers[i].port + '/system/stats?type=simple')
             .success(function(data, status, headers, config) {
-                $scope.users = data;
-                $scope.$apply();
+                $scope.servers[i].online  = data.online;
+                $scope.servers[i].freemem = data.freemem;
             })
             .error(function(data, status, headers, config) {
-                $scope.users = data;
-                $scope.$apply();
-            })
-        ;
-    };
+                console.log(data);
+            });
+        }
+    })
+    .error(function(data, status, headers, config) {
+        console.log(data);
+    });
 }]);
 
-app.controller('ServerCtlr', [
-        '$scope',
-        '$rootScope',
-        '$http',
-        '$routeParams',
-        '$location',
-        function($scope, $rootScope, $http, $routeParams, $location) {
+app.controller('ServerManagementCtlr', ['$scope', '$http', function($scope, $http) {
     $scope.pageSize    = 20;
     $scope.currentPage = 0;
-    $scope.terminalResponse = '';
-    $scope.serviceStatus = [];
+    $scope.installerResponse = '';
 
-    function getConnectionDetails(callback) {
-        if (!$rootScope.server) {
-            $http.get('/api/server/' + $routeParams.hostname + '/')
-                .success(function(data, status, headers, config) {
-                    $rootScope.server = data;
-                    callback();
-                })
-                .error(function(data, status, headers, config) {
-                    console.log(data);
-                })
-            ;
-        } else {
-            callback();
-        }
-    }
-
-    $scope.init = function(callback) {
-        getConnectionDetails(function() {
-            createSocket($rootScope.server.host, $rootScope.server.port, function(socket) {
-                // $scope.socket is stored in scope for garbage collection purposes
-                $scope.socket = socket;
-
-                $scope.socket.on('start service response', function(service, result) {
-                    if (result == 'success') toastr.success(service + ' started successfully');
-                    if (result == 'failure') toarts.error(service + ' could not be started');
-
-                    if (!!$scope.service) $scope.getServiceInfo(service);
-                    if (!!$scope.serviceStatus) $scope.getServiceStatus(service);
-                });
-
-                $scope.socket.on('stop service response', function(service, result) {
-                    if (result == 'success') toastr.success(service + ' stopped successfully');
-                    if (result == 'failure') toastr.error(service + ' could not be stopped');
-
-                    if (!!$scope.service) $scope.getServiceInfo(service);
-                    if (!!$scope.serviceStatus) $scope.getServiceStatus(service);
-                });
-
-                $scope.socket.on('restart service response', function(service, result) {
-                    if (result == 'success') toastr.success(service + ' restarted successfully');
-                    if (result == 'failure') toastr.error(service + ' could not be restarted');
-
-                    if (!!$scope.service) $scope.getServiceInfo(service);
-                    if (!!$scope.serviceStatus) $scope.getServiceStatus(service);
-                });
-
-                $scope.socket.on('password required', function(operation, user) {
-                    toastr.warning('Password required to ' + operation + ' with user ' + user);
-
-                    swal({
-                        title: 'Password required',
-                        text: 'A password is required to complete this operation',
-                        type: 'input',
-                        inputType: 'password',
-                        showCancelButton: true,
-                        closeOnConfirm: true,
-                        animation: 'slide-from-top',
-                        inputPlaceholder: 'Password'
-                    }, function(password){
-                        if (password === false) return false;
-                        else if (password === '') {
-                            swal.showInputError('Password is required');
-                            return false;
-                        }
-                        else {
-                            $scope.socket.emit('password supplied', password);
-                        }
-                    });
-                });
-
-                $scope.socket.on('stdout', function(data) {
-                    $scope.terminalResponse += data;
-                    console.log('STDOUT:  ' + data);
-                    $scope.$apply();
-                });
-
-                $scope.socket.on('stderr', function(data) {
-                    $scope.terminalResponse += data;
-                    console.log('STDERR:  ' + data);
-                    $scope.$apply();
-                });
-
-                $scope.socket.on('error', function(data) {
-                    toastr.error('data');
-                });
-
-                $scope.socket.on('service status response', function(service, status) {
-                    for (var i = 0; i < $scope.serviceStatus.length; i++) {
-                        if ($scope.serviceStatus[i].service == service) $scope.serviceStatus[i].isRunning = status;
-                    }
-                    $scope.$apply();
-                });
-
-                $scope.socket.on('service status all response', function(service, status) {
-                    $scope.serviceStatus.push({
-                        service: service,
-                        isRunning: status
-                    });
-                    $scope.$apply();
-                });
-
-                callback();
-            });
-        });
-    };
+    var socket = io('http://' + host + ':' + port);
 
     $scope.getStats = function() {
-        $http.get('//' + $rootScope.server.host + ':' + $rootScope.server.port + '/api/system/stats?type=all')
+        $http.get('//' + host + ':' + port + '/system/stats?type=all')
             .success(function(data, status, headers, config) {
                 $scope.server = data;
 
@@ -280,8 +57,7 @@ app.controller('ServerCtlr', [
             })
             .error(function(data, status, headers, config) {
                 console.log(data);
-            })
-        ;
+            });
     };
 
     $scope.getPlatformClass = function(platform) {
@@ -289,12 +65,13 @@ app.controller('ServerCtlr', [
         if (platform == 'windows') return 'fa fa-windows';
         if (platform == 'apple')   return 'fa fa-wheelchair';
     };
+
     $scope.loadAvg = function() {
         var transform_styles = ['-webkit-transform',
             '-ms-transform'];
 
-        for (var i in $rootScope.server.loadavg) {
-            var rotation = Math.floor($rootScope.server.loadavg[i] / $rootScope.server.cpu.length * 180);
+        for (var i in $scope.server.loadavg) {
+            var rotation = Math.floor($scope.server.loadavg[i] / $scope.server.cpu.length * 180);
             var fix_rotation = rotation * 2;
             for (var j in transform_styles) {
                 $('#circle-'+i+' .fill, #circle-'+i+' .mask.full').css(transform_styles[j], 'rotate(' + rotation + 'deg)');
@@ -303,93 +80,53 @@ app.controller('ServerCtlr', [
         }
     };
 
-    $scope.getPkgs = function() {
-        $http.get('//' + $rootScope.server.host + ':' + $rootScope.server.port + '/api/worker/packages/list')
+    $scope.getPackages = function() {
+        $http.get('//' + host + ':' + port + '/worker/packages/list')
             .success(function(data, status, headers, config) {
-                $scope.pkgs = data;
+                $scope.packages = data;
             })
             .error(function(data, status, headers, config) {
-                $scope.pkgs = data;
-            })
-        ;
+                $scope.packages = data;
+            });
     };
 
-    $scope.getPkgManagers = function() {
-        $http.get('//' + $rootScope.server.host + ':' + $rootScope.server.port + '/api/worker/packages/listManagers')
+    $scope.getPackageManagers = function() {
+        $http.get('//' + host + ':' + 9000 + '/worker/packages/listManagers')
             .success(function(data, status, headers, config) {
                 $scope.managers = data;
             })
             .error(function(data, status, headers, config) {
                 $scope.managers = data;
-            })
-        ;
+            });
     };
 
-    $scope.getPkgInfo = function(pkg) {
-        $http.get('//' + $rootScope.server.host + ':' + $rootScope.server.port + '/api/worker/packages/getInfo/' + pkg)
-            .success(function(data, status, headers, config) {
-                $scope.pkg = data;
-            })
-            .error(function(data, status, headers, config) {
-                $scope.pkg = data;
-            })
-        ;
-    };
-
-    $scope.installPkg = function() {
-        $scope.socket.emit('install package', {
+    $scope.installPackage = function() {
+        socket.emit('install package', {
             manager: $scope.pkgMngr,
-            pkg: $scope.pkgInstallQuery
+            package: $scope.pkgInslQuery
         });
     };
 
-    $scope.updatePkg = function() {
-        $scope.socket.emit('update package', {
-            manager: $scope.pkgMngr,
-            pkg: $scope.pkgUpdateQuery
-        });
-    };
-
-    $scope.getServiceStatus = function(service) {
-        $scope.socket.emit('get service status', service);
-    };
-
-    $scope.getServiceInfo = function(service) {
-        $http.get('//' + $rootScope.server.host + ':' + $rootScope.server.port + '/api/worker/services/getInfo/' + service)
-            .success(function(data, status, headers, config) {
-                $scope.service = data;
-                $scope.$apply();
-            })
-            .error(function(data, status, headers, config) {
-                $scope.service = data;
-                $scope.$apply();
-            })
-        ;
-    };
-
-    $scope.getRunningServices = function() {
-        $scope.socket.emit('get status all');
-    };
-
-    $scope.startService = function(target) {
-        toastr.info('Starting service ' + target + '...');
-        $scope.socket.emit('start service', target);
-    };
-
-    $scope.stopService = function(target) {
-        toastr.info('Stopping service ' + target + '...');
-        $scope.socket.emit('stop service', target);
-    };
-
-    $scope.restartService = function(target) {
-        toastr.info('Restarting service ' + target + '...');
-        $scope.socket.emit('restart service', target);
-    };
+    socket.on('stdout', function(data) {
+        $scope.installerResponse += data;
+        console.log('STDOUT:  ' + data);
+        $scope.$apply();
+    });
+    socket.on('stderr', function(data) {
+        $scope.installerResponse += data;
+        console.log('STDERR:  ' + data);
+        $scope.$apply();
+    });
+    socket.on('error', function(data) {
+        $scope.installerResponse += data;
+        console.log('ERROR:  ' + data);
+        $scope.$apply();
+    });
 
     $scope.sendInput = function() {
-        $scope.terminalResponse += '\n';
-        $scope.socket.emit('input', { input: $scope.terminalInput });
-        $scope.terminalInput = '';
+        $scope.installerResponse += '\n';
+        socket.emit('input', { input: $scope.installerInput });
+        $scope.installerInput = '';
     };
 }]);
 
@@ -407,10 +144,7 @@ app.filter('bytes', function() {
 
 app.filter('offsetBy', function() {
     return function(input, start) {
-        if (!!input) {
-            start = + start;
-            return input.slice(start);
-        }
-        return [];
+        start = +start;
+        return input.slice(start);
     };
 });
