8eaa9dbe0016463966da2067affdfa0ff164c3f5
Keyton Stanier <keyton.j.stanier@gmail.com>
2015-08-01 20:38:19 -0400

    Cleaned up list.js

--- src/scripts/controllers/server/packages/list.js
+++ src/scripts/controllers/server/packages/list.js
@@ -3,34 +3,25 @@ app.controller('PackageListCtlr', [
     '$rootScope',
     '$routeParams',
     '$http',
-    function($scope, $rootScope, $routeParams, $http) {
-        function init(callback) {
-            if (!$rootScope.server) {
-                $http.get('/api/server/' + $routeParams.hostname + '/')
-                    .success(function(data, status, headers, config) {
-                        $rootScope.server = data;
-                        callback();
-                    })
-                    .error(function(data, status, headers, config) {
-                        console.log(data);
-                    })
-                ;
-            } else {
-                callback();
-            }
-        }
+    'ServerFactory',
+    function($scope, $rootScope, $routeParams, $http, ServerFactory) {
+        $scope.pkgs = [];
 
         $scope.getPkgs = function() {
-            $http.get('//' + $rootScope.server.host + ':' + $rootScope.server.port + '/api/worker/packages/list')
-                .success(function(data, status, headers, config) {
-                    $scope.pkgs = data;
-                })
-                .error(function(data, status, headers, config) {
-                    $scope.$apply(function() {
-                        $scope.pkgs = data;
-                    });
-                })
-            ;
+            $scope.$on('serverInfoReady', function() {
+                $http.get('//' + $rootScope.server.host + ':' + $rootScope.server.port + '/api/worker/packages/list')
+                    .success(function(data, status, headers, config) {
+                        $scope.$apply(function() {
+                            for (var i in data) $scope.pkgs.push(data[i]);
+                        });
+                    })
+                    .error(function(data, status, headers, config) {
+                        $scope.$apply(function() {
+                            $scope.pkgs = data;
+                        });
+                    })
+                ;
+            });
         };
     }
 ]);
