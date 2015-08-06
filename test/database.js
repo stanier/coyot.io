d8a3337eaafbff3ddb396eef3ef841165e450238
Keyton Stanier <keyton.j.stanier@gmail.com>
2015-08-06 17:29:21 -0400

    Added groupModel tests

--- test/database.js
+++ test/database.js
@@ -2,18 +2,19 @@ var assert = require('chai').assert;
 
 var mongoose = require('mongoose');
 
-var db = require('../lib/dbschema'),
-    userModel = db.userModel,
-    serverModel = db.serverModel;
+var db          = require('../lib/dbschema'),
+    userModel   = db.userModel,
+    serverModel = db.serverModel,
+    groupModel  = db.groupModel;
 
 describe('Database', function() {
+    before('connect to test database', function(done) {
+        if (mongoose.connection.db) return done();
+
+        mongoose.connect('mongodb://localhost:27017/coyotio_mocha', done);
+    });
+
     describe('UserModel', function() {
-        before('connect to test database', function(done) {
-            if (mongoose.connection.db) return done();
-
-            mongoose.connect('mongodb://localhost:27017/coyotio_mocha', done);
-        });
-
         describe('create', function() {
             it('should create a new user without error', function(done) {
                 var user = {
@@ -87,7 +88,7 @@ describe('Database', function() {
             });
         });
 
-        after('remove all userModel entries', function(done) {
+        after('remove all userModel documents', function(done) {
             userModel.remove({}, function(err) {
                 if (err) throw err;
 
@@ -96,8 +97,62 @@ describe('Database', function() {
         });
     });
 
+    describe('GroupModel', function() {
+        describe('create', function() {
+            it('should create a new group without error', function(done) {
+                var group = {
+                    name: 'bobs crew',
+                    description: 'the most awesome crew to ever not exist'
+                };
+
+                groupModel.create(group, function(err) {
+                    if (err) throw err;
+
+                    done();
+                });
+            });
+        });
+
+        describe('findOne', function() {
+            it('should return name, description of user', function(done) {
+                groupModel
+                    .findOne({ name: 'bobs crew' })
+                    .select('name description')
+                    .exec(function(err, result) {
+                        if (err) throw err;
+
+                        done();
+                    })
+                ;
+            });
+        });
+
+        describe('update', function() {
+            it('should compare data and update group', function(done) {
+                var data = { description: 'bobs lonely crew' };
+
+                groupModel
+                    .findOne({ name: 'bobs crew' })
+                    .update(data, function(err) {
+                        if (err) throw err;
+
+                        done();
+                    })
+                ;
+            });
+        });
+
+        after('remove all groupModel documents', function(done) {
+            groupModel.remove({}, function(err) {
+                if (err) throw err;
+
+                done();
+            });
+        });
+    });
+
     describe('ServerModel', function() {
-        after('remove all serverModel entries', function(done) {
+        after('remove all serverModel documents', function(done) {
             serverModel.remove({}, function(err) {
                 if (err) throw err;
 
