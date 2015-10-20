4ceae1c7a85e77c48ca0bd5883f805bd948f01bd
Keyton Stanier <keyton.j.stanier@gmail.com>
2015-10-19 20:46:06 -0400

    Fixed permissions management for groups

--- src/scripts/controllers/management/group/permissions.js
+++ src/scripts/controllers/management/group/permissions.js
@@ -3,6 +3,8 @@ app.controller('GroupPermsCtlr', [
     '$http',
     '$stateParams',
     function($scope, $http, $stateParams) {
+        $scope.selected = {};
+
         $scope.group = {
             name: $stateParams.group
         };
@@ -12,7 +14,6 @@ app.controller('GroupPermsCtlr', [
             .success(function(data, status, headers, config) {
                 if (data.success) {
                     $scope.categories = data.result;
-                    console.log(data.result);
                 }
                 else toastr.error(data.error);
             })
@@ -26,7 +27,6 @@ app.controller('GroupPermsCtlr', [
             .success(function(data, status, headers, config) {
                 if (data.success) {
                     $scope.group.permissions = data.result;
-                    console.log(data.result);
                 } else toastr.error(data.error);
             })
             .error(function(data, status, headers, config) {
@@ -34,8 +34,41 @@ app.controller('GroupPermsCtlr', [
             })
         ;
 
-        $scope.update = function() {
+        $scope.change = function(handle) {
+            if ($scope.group.permissions.indexOf(handle) > -1) {
+                $http.delete('/api/management/groups/' + $stateParams.group + '/permissions/' + handle);
+                checkPermission(handle);
+            } else {
+                $http.put('/api/management/groups/' + $stateParams.group + '/permissions/' + handle);
+                checkPermission(handle);
+            }
 
+            console.log($scope.group.permissions);
         };
+
+        $scope.update = function() {
+            $http.post('/api/management/groups/' + $stateparams.group + '/permissions', postData)
+                .success(function(data, status, headers, config) {
+                    toastr.success(data);
+                })
+                .error(function(data, status, headers, config) {
+                    toastr.error(data);
+                })
+            ;
+        };
+
+        function checkPermission(handle) {
+            $http.get('/api/management/groups/' + $stateParams.group + '/permissions/' + handle)
+                .success(function(data, status, headers, config) {
+                    if (data.result === true && $scope.group.permissions.indexOf(handle) < 0)
+                        $scope.group.permissions.push(handle);
+                    else if (data.result === false && $scope.group.permissions.indexOf(handle) > -1)
+                        $scope.group.permissions.splice($scope.group.permissions.indexOf(handle));
+                })
+                .error(function(data, status, headers, config) {
+                    console.log(data);
+                })
+            ;
+        }
     }
 ]);
