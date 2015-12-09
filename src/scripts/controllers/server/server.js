2dc787a212a7179297057ebce2d83bdd32d05359
Keyton Stanier <keyton.j.stanier@gmail.com>
2015-12-09 18:01:03 -0500

    Removed deprecated getStats()

--- src/scripts/controllers/server/server.js
+++ src/scripts/controllers/server/server.js
@@ -10,31 +10,10 @@ app.controller('ServerCtlr', [
         $scope.terminalResponse = '';
         $scope.serviceStatus = [];
 
-        $scope.getStats = function() {
-            $scope.loadAvg();
-        };
-
         $scope.getPlatformClass = function(platform) {
             if (platform == 'linux')   return 'fa fa-linux';
             if (platform == 'windows') return 'fa fa-windows';
             if (platform == 'darwin')  return 'fa fa-wheelchair';
-        };
-
-        $scope.loadAvg = function() {
-            var transform_styles = [
-                '-webkit-transform',
-                '-ms-transform'
-            ];
-
-            for (var i in $rootScope.server.loadavg) {
-                var rotation = Math.floor($rootScope.server.loadavg[i] / $rootScope.server.cpu.length * 180);
-                var fix_rotation = rotation * 2;
-
-                /*for (var j in transform_styles) {
-                    $('#circle-'+i+' .fill, #circle-'+i+' .mask.full').css(transform_styles[j], 'rotate(' + rotation + 'deg)');
-                    $('#circle-'+i+' .fill.fix').css(transform_styles[j], 'rotate(' + fix_rotation + 'deg)');
-                }*/
-            }
         };
 
         $scope.sendInput = function() {
