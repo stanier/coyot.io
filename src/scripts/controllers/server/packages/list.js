4acf3cafa7292f51e6e4c5baa2df5a030ceaef5c
Keyton Stanier <keyton.j.stanier@gmail.com>
2016-01-28 19:10:09 -0500

    Improved pagination
    Needs work, pagination won't actually fire on clicking chevron/arrow

--- src/scripts/controllers/server/packages/list.js
+++ src/scripts/controllers/server/packages/list.js
@@ -6,24 +6,35 @@ app.controller('PackageListCtlr', [
         $scope.query = {
             order: 'name',
             limit: 15,
-            page: 0
+            page: 1
         };
 
         $scope.pkgs = [];
+        $scope.pkgCount = 1;
 
         $scope.getPkgs = function() {
-            $http.get('//' + $rootScope.server.host + ':' + $rootScope.server.port + '/api/worker/packages/list')
+            $http.get('//' + $rootScope.server.host + ':' + $rootScope.server.port +
+                    '/api/worker/packages/list?offset=' + ($scope.query.limit * ($scope.query.page - 1)) +
+                    '&max=' + $scope.query.limit)
                 .success(function(data, status, headers, config) {
-                    $scope.pkgs = data;
+                    $scope.pkgs = data.list;
+                    $scope.pkgCount = data.count;
                 })
                 .error(function(data, status, headers, config) {
-                    $scope.pkgs = data;
+                    $scope.pkgs = data.list;
+                    $scope.pkgCount = data.count;
                 })
             ;
         };
 
-        $scope.onPaginate = function() {
+        $scope.flip = function() {
+            console.log('fuck');
+            //$scope.query.limit = limit;
+            //$scope.query.page = page;
 
+            console.log('bitch');
+
+            $scope.getPkgs();
         };
 
         if (!!$rootScope.server) {
